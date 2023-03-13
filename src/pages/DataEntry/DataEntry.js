f71bfdb233c05dd12fbe486c52de7edeeb665717
Aneri Sheth <anerisheth1@gmail.com>
2023-03-13 02:17:47 -0700

    Data Entry

--- src/pages/DataEntry/DataEntry.js
+++ src/pages/DataEntry/DataEntry.js
@@ -1,14 +1,55 @@
 import './DataEntry.css'
 import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
 
 export default function DataEntry(){
     const navigate = useNavigate()
+    const totalBudget = 1000;
+    const [budget, setBudget] = useState(0)
+    const [cfClicks, setcfcClicks] = useState(0);
+    const [ctcClicks, setctcClicks] = useState(0);
+    const [hncClicks, sethncClickes] = useState(0);
+  
+
+    function countClicks(set, cereal) {
+        set(cereal);
+
+    }
+
+    function budgetTracker(set, price){
+        set(totalBudget-price);
+        totalBudget = totalBudget - price;
+    }
+        
+    function run(set, price, click){
+        countClicks(set, price);
+        budgetTracker(setBudget, price);
+    }
     return (
         <div>
-        <button onClick={() => navigate('/recommendation')}>
+        <button onClick={() => navigate('/recommendation', {state: {cfClicks, ctcClicks, hncClicks}})}>
         Click to go to Recommendation
         </button>
-        <p> This is the DataEntry page</p>
+        <p> Welcome to our Cereal Budget App!</p>
+        <p>Your total budget is: ${budget}</p>
+
+        <button onClick={() => run(setcfcClicks, 3, cfClicks)}>
+        Buy Corn Flakes for $3
+        </button>
+        <p>You bought ${cfClicks} of Corn Flakes!</p>
+
+
+        <button onClick={() => run(setctcClicks, 4, ctcClicks)}>
+        Buy Cinnamon Toast Crunch for $4
+        </button>
+        <p>You bought ${ctcClicks} of Cinnamon Toast Crunch!</p>
+
+        <button onClick={() => run(sethncClickes, 2, hncClicks)}>
+        Buy Honey Nut Cheerios for $2
+        </button>
+        <p>You bought ${hncClicks} of Honey Nut Cheerios</p>
+
         </div>
+
     )
 }
--- src/pages/Recommendations/Recommendation.js
+++ src/pages/Recommendations/Recommendation.js
@@ -1,8 +1,10 @@
 import './Recommendation.css'
 import { useNavigate } from 'react-router-dom'
+import { useLocation } from "react-router-dom";
 
 export default function Recommendation(){
     const navigate  = useNavigate()
+    //const { cfClicks, ctcClicks, hncClicks } = location.state;
     return (
         <div>
         <button onClick={() => navigate('/')}>Click to go to DataEntry</button>
